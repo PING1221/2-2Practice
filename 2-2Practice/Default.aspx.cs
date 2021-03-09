7fef20539a8c0fc572a0fb3efcaf7c4d9b1f0717
君蘋 魏 <107111205@mail.oit.edu.tw>
2021-03-09 17:17:13 +0800

    閏年

--- 2-2Practice/Default.aspx.cs
+++ 2-2Practice/Default.aspx.cs
@@ -5,10 +5,39 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-namespace _2_2Practice {
-    public partial class Default : System.Web.UI.Page {
-        protected void Page_Load(object sender, EventArgs e) {
+namespace _2_2Practice
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            int year = 1000;
+            bool b_IsLeap = false;
+            if (year % 4 == 0)
+            {
+                b_IsLeap = true;
 
+                if (year % 100 == 0)
+                {
+                    if (year % 400 == 0)
+                    {
+                        b_IsLeap = true;
+                    }
+                    else
+                    {
+                        b_IsLeap = false;
+                    }
+                }
+
+                if (b_IsLeap == true)
+                {
+                    Response.Write("Y");
+                }
+                else
+                {
+                    Response.Write("N");
+                }
+            }
         }
     }
 }
